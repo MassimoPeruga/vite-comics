6c2cce9a8dd0d35a4c04261516432b65db12b1ff
MassimoPeruga <spamma-mutevole0p@icloud.com>
2023-12-11 22:54:27 +0100

    feat: Content

---
+++ src/components/MainComponents/Card.vue
@@ -0,0 +1,19 @@
+<script >
+export default {
+    name: 'Card',
+    props: {
+        image: String,
+        title: String,
+    }
+};
+</script>
+
+<template>
+    <li>
+        <div class="image-container">
+            <img :src="image" :alt="title">
+        </div>
+        <h4>{{ title }}</h4>
+    </li>
+</template>
+<style lang="scss" scoped></style>
--- src/components/MainComponents/Jumbotron.vue
+++ src/components/MainComponents/Jumbotron.vue
@@ -8,30 +8,12 @@ export default {
 <template>
     <section>
         <img src="/jumbotron.jpg" alt="Jumbo">
-        <div class="container">
-            <h3>Current Series</h3>
-        </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
-@use '../../assets/scss/partials/variables' as *;
-
 section {
     height: 400px;
-
-    h3 {
-        display: inline-block;
-        background-color: $blue-dc;
-        padding: 0 20px;
-        height: 60px;
-        line-height: 60px;
-        text-transform: uppercase;
-        color: white;
-        position: relative;
-        bottom: 30px;
-        left: 0;
-    }
 
     img {
         width: 100%;
